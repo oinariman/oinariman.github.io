06625779148d3333b1c89837e2c5a5ddfedb85c0
Ryosuke Mihara <mihara.ryosuke@gmail.com>
2016-11-05 20:27:54 +0900

    Updated dev progress

--- gesuido/js/progress_chart.js
+++ gesuido/js/progress_chart.js
@@ -13,15 +13,15 @@ var colors = [
 
 // data
 
-var dates = ["2016/10/23", "2016/10/30"];
+var dates = ["2016/10/23", "2016/10/30", "2016/11/5"];
 var data = [
-    [42.35, 43.50],
-    [51.11, 51.11],
-    [12.0, 25.91],
-    [70.0, 70.0],
-    [25.0, 25.0],
-    [35.0, 35.0],
-    [40.71, 40.75]
+    [42.35, 43.50, 33.59],
+    [51.11, 51.11, 61.69],
+    [12.0, 25.91, 17.38],
+    [70.0, 70.0, 68.0],
+    [25.0, 25.0, 33.33],
+    [35.0, 35.0, 38.18],
+    [40.71, 40.75, 38.28]
 ]
 
 // insert values
@@ -43,6 +43,7 @@ for (var i = 0; i < labels.length; i++) {
     dataset.data = data[i];
     dataset.borderColor = colors[i];
     dataset.borderWidth = 1;
+    dataset.lineTension = 0.0;
     if ( i == labels.length - 1 ) { dataset.borderWidth = 4; }
     datasets.push(dataset);
 }
