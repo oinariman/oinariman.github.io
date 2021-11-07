80297d2dbb60f8f06044506c7d231796a5c0bf6b
三原亮介 <r_mihara@waku-2.com>
2021-11-07 16:38:40 +0900

    Updated obituaries pages

--- gesuido/js/cod.js
+++ gesuido/js/cod.js
@@ -5,12 +5,47 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var strings = {
+  en: {
+    "class-investigator": "Investigator",
+    "class-ronin": "Ronin",
+    "class-programmer": "Programmer",
+    "level": "Lv. ",
+    "elapsed-time-1" : "Elapsed time: approx. ",
+    "elapsed-time-2" : " turns",
+    "build": "Build: ",
+    "id": "ID: "
+  },
+  ja: {
+    "class-investigator": "調査員",
+    "class-ronin": "浪人",
+    "class-programmer": "プログラマー",
+    "level": "Lv. ",
+    "elapsed-time-1" : "生存時間: 約 ",
+    "elapsed-time-2" : " ターン",
+    "build": "ビルド: ",
+    "id": "ID: "
+  },
+}
+
+function l(key) {
+  return strings[languageCode][key];
+}
+
 function addCOD(data) {
-  if (data.player_id == "dev") { return }
-  let row = "<li>" + data.cause_of_death + "&nbsp;<span>" + data.created_at.toDate().toTwitterRelativeTime('ja') + "</span>";
-  row += "<ul class=\"sub_list\"><li>Lv. " + data.survival_level + " / ";
-  row += "生存時間: 約 " + data.elapsed_time / 8 + " ターン / ";
-  row += "ID: " + data.player_id.substring(0, 4) + "********</li></ul></li>";
+  if (languageCode == undefined || data.player_id == "dev") return;
+  let cause_of_death = data['cause_of_death_' + languageCode];
+  if (cause_of_death == undefined) cause_of_death = data.cause_of_death;
+  if (cause_of_death == undefined) return;
+  let row = "<li>" + cause_of_death + "&nbsp;<span>" + data.created_at.toDate().toTwitterRelativeTime(languageCode) + "</span>";
+  row += "<ul class=\"sub_list\"><li>";
+  if (data.class_name != undefined) {
+    row += l("class-" + data.class_name) + " / ";
+  }
+  row += l("level") + data.survival_level + " / ";
+  row += l("elapsed-time-1") + data.elapsed_time / 8 + l("elapsed-time-2") + " / ";
+  row += l("build") + data.build + " / ";
+  row += l("id") + data.player_id.substring(0, 4) + "********</li></ul></li>";
   $("#list").append(row);
 }
 
